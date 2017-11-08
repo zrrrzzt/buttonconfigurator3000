8dd311ad9e5480c3a89d4e6c16485711ab7990bc
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-08 18:42:02 +0100

    Updates config and README

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = {
-  API_KEY: process.env.API_KEY || 'Your secret key'
+  API_KEY: process.env.API_KEY || 'Your secret key',
+  SITE_URL: process.env.SITE_URL || 'http://nodeconfeubadge.allthethings.win'
 }
